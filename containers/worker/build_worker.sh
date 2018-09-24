f32bc695135443cae529b42b89ad8e83382991fc
ruigrafino <babelgrafino@gmail.com>
2018-09-24 17:05:10 +0100

    Changed some containers build files

--- containers/worker/build_worker.sh
+++ containers/worker/build_worker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+#
+# MAINTAINER: Rui Grafino
+# DATE: 19/09/2018
+# VERSION: v1.0
 
 cp -r ../../MultiVAC .
-docker build -t multivac_worker:v1 . && rm -rf Multivac
 
+CONTAINER_REGISTRY="registry.gitlab.com/ruigrafino/project"
+CONTAINER_NAME="multivac-worker"
+docker build -t "${CONTAINER_NAME}" .
+docker build -t "${CONTAINER_REGISTRY}:${CONTAINER_NAME}" . && docker push "${CONTAINER_REGISTRY}:${CONTAINER_NAME}"
